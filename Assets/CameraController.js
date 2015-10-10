71be5685e35673d5f391cee557a464923ca585e7
Ken Hoff <thekenhoff@gmail.com>
2015-10-10 09:19:08 -0700

    rudimentary runner

---
+++ Assets/CameraController.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var FollowObject : Transform;
+
+private var initialOffset : Vector3;
+
+function Awake () {
+	initialOffset = transform.position - FollowObject.position;
+}
+
+function Update () {
+	if (FollowObject) {
+		transform.position = FollowObject.position + initialOffset;
+	}
+}
---
+++ Assets/PlayerController.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+public var acceleration : float = 1.0;
+public var maxSpeed : float = 5;
+public var startingHP : float = 20;
+
+private var rb : Rigidbody2D;
+
+function Awake () {
+	rb = GetComponent.<Rigidbody2D>();
+
+}
+
+function FixedUpdate () {
+	rb.AddForce(Vector2(acceleration * Time.fixedDeltaTime, 0));
+	if (rb.velocity.x > maxSpeed) {
+		rb.velocity.x = maxSpeed;
+	}
+}
+
+function Update() {
+	if (Input.GetButtonDown("A")) {
+		Debug.Log("Pressed A!");
+	}
+	startingHP -= Time.deltaTime;
+	Debug.Log(startingHP);
+	if (startingHP <= 0) {
+		Destroy(gameObject);
+	}
+}
